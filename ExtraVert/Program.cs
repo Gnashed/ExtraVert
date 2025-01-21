19d8e53e0d321ece42ba0ffb220caa1e2bcd255f
Tion Blackmon <tion.blackmon@outlook.com>
2025-01-21 17:50:17 -0600

    Created a method that includes most of the logic for the menu.

--- ExtraVert/Program.cs
+++ ExtraVert/Program.cs
@@ -53,57 +53,106 @@ List<Plant> plants = new List<Plant>()
     }
 };
 
-// STARTUP GREETING AND PROMPT USER TO MAKE A SELECTION IN THE MENU.
-Console.WriteLine("Welcome to Tion's Plant Shop!");
-Console.WriteLine();
-
 // MENU
-Console.WriteLine("Please choose one of the following options: ");
-Console.WriteLine("a: Display all plants.");
-Console.WriteLine("b: Post a plant to be adopted.");
-Console.WriteLine("c: Adopt a plant today!");
-Console.WriteLine("d: De-list a plant.");
-Console.WriteLine("e: Exit the program.");
-string response = Console.ReadLine().Trim();
-
-while (response != "e")
+void plantMenu()
 {
-    
+    Console.WriteLine("Please choose one of the following options: ");
+    Console.WriteLine("a: Display all plants.");
+    Console.WriteLine("b: Post a plant to be adopted.");
+    Console.WriteLine("c: Adopt a plant today!");
+    Console.WriteLine("d: De-list a plant.");
+    Console.WriteLine("e: Exit the program.");
+    string response = Console.ReadLine().Trim();
+
+    // PROCESS MENU RESPONSE
+    switch (response?.ToLower())
+    {
+        case "a":
+            Console.WriteLine("Showing all plants...");
+            foreach (Plant plant in plants)
+            { 
+                Console.WriteLine(plant.Species);
+                Console.WriteLine(plant.LightNeeds);
+                Console.WriteLine(plant.AskingPrice);
+                Console.WriteLine(plant.City);
+                Console.WriteLine(plant.ZipCode);
+                Console.WriteLine($"Sold out? {plant.isSold}");
+                Console.WriteLine($"Regulated? {plant.isRegulated}");
+                Console.WriteLine();
+                Console.WriteLine();
+            } 
+            plantMenu();
+            break;
+        case "b":
+            Console.WriteLine("Post a plant to be adopted...");
+            break;
+        case "c":
+            Console.WriteLine("Which plant do you want to adopt?");
+            break;
+        case "d":
+            Console.WriteLine("Select a plant to de-list:");
+            break;
+        case "e":
+            Console.WriteLine("Terminating program...");
+            break;
+        default:
+            Console.WriteLine("Invalid response. Please try again. ");
+            Console.WriteLine();
+            plantMenu();
+            break;
+    };
 }
 
-// PROCESS MENU RESPONSE
-switch (response?.ToLower())
-{
-    case "a":
-        Console.WriteLine("Showing all plants...");
-        break;
-    case "b":
-        Console.WriteLine("Post a plant to be adopted...");
-        break;
-    case "c":
-        Console.WriteLine("Which plant do you want to adopt?");
-        break;
-    case "d":
-        Console.WriteLine("Select a plant to de-list:");
-        break;
-    case "e":
-        Console.WriteLine("Terminating program...");
-        break;
-    default:
-        Console.WriteLine("Invalid response. Please try again. ");
-        break;
-};
+// STARTUP GREETING AND PROMPT USER TO MAKE A SELECTION IN THE MENU.
+string greeting = "Welcome to Tion's Plant Shop!";
+Console.WriteLine(greeting);
+plantMenu();
 
-// Loop through plants
-// foreach (Plant plant in plants)
+#region beforeTurningToMethod
+// STARTUP GREETING AND PROMPT USER TO MAKE A SELECTION IN THE MENU.
+// string greeting = "Welcome to Tion's Plant Shop!";
+// Console.WriteLine(greeting);
+// plantMenu();
+
+// Console.WriteLine("Please choose one of the following options: ");
+// Console.WriteLine("a: Display all plants.");
+// Console.WriteLine("b: Post a plant to be adopted.");
+// Console.WriteLine("c: Adopt a plant today!");
+// Console.WriteLine("d: De-list a plant.");
+// Console.WriteLine("e: Exit the program.");
+// string response = Console.ReadLine().Trim();
+//
+// // PROCESS MENU RESPONSE
+// switch (response?.ToLower())
 // {
-//     Console.WriteLine(plant.Species);
-//     Console.WriteLine(plant.LightNeeds);
-//     Console.WriteLine(plant.AskingPrice);
-//     Console.WriteLine(plant.City);
-//     Console.WriteLine(plant.ZipCode);
-//     Console.WriteLine($"Sold out? {plant.isSold}");
-//     Console.WriteLine($"Regulated? {plant.isRegulated}");
-//     Console.WriteLine();
-//     Console.WriteLine();
-// }
+//     case "a":
+//         Console.WriteLine("Showing all plants...");
+//         foreach (Plant plant in plants)
+//         { 
+//             Console.WriteLine(plant.Species);
+//             Console.WriteLine(plant.LightNeeds);
+//             Console.WriteLine(plant.AskingPrice);
+//             Console.WriteLine(plant.City);
+//             Console.WriteLine(plant.ZipCode);
+//             Console.WriteLine($"Sold out? {plant.isSold}");
+//             Console.WriteLine($"Regulated? {plant.isRegulated}");
+//             Console.WriteLine();
+//             Console.WriteLine();
+//         } break;
+//     case "b":
+//         Console.WriteLine("Post a plant to be adopted...");
+//         break;
+//     case "c":
+//         Console.WriteLine("Which plant do you want to adopt?");
+//         break;
+//     case "d":
+//         Console.WriteLine("Select a plant to de-list:");
+//         break;
+//     case "e":
+//         Console.WriteLine("Terminating program...");
+//         break;
+//     default:
+//         Console.WriteLine("Invalid response. Please try again. ");
+//         break;
+// };
+#endregion
